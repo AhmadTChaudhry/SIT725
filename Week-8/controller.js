44148e844a343312f4e2f05b3b7b7670368fee04
Ahmad Chaudhry <s224227027@deakin.edu.au>
2024-09-06 23:10:57 +1000

    project setup with docker

---
+++ Week-8/controller.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const { connectToDB, insertFormData } = require('./model');
+const { Server } = require('socket.io');
+
+const app = express();
+const port = 3040;
+
+// Start the server
+const server = app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+
+const io = new Server(server);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+connectToDB().catch(console.dir);
+
+let userCount = 0; // Tracking number of users that are connected
+
+app.post('/submit-form', async (req, res) => {
+    try {
+        const formData = req.body;
+        console.log("Received form data:\n", formData);
+        await insertFormData(formData);
+
+
+        res.json({ message: 'Form submitted successfully! Someone will reach out to you :)' });
+    } catch (error) {
+        console.error("Error inserting form data:", error);
+        res.status(500).json({ message: 'Failed to submit form' });
+    }
+});
+
+io.on('connection', (socket) => {
+    userCount++;
+    console.log(`User connected. \nTotal connected users users: ${userCount}`);
+
+    socket.on('disconnect', () => {
+        userCount--;
+        console.log(`User disconnected.\nTotal connected users: ${userCount}`);
+    });
+});
+
+
+
